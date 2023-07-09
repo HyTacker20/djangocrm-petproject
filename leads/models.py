c5e551ca116a1983b30acf65cc1ee44805819b55
Andrii Hermak <hermak20102004@gmail.com>
2023-07-09 11:25:34 +0200

    feat: add Agent model

--- leads/models.py
+++ leads/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    pass
 
 
 class Lead(models.Model):
@@ -10,9 +15,8 @@ class Lead(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.PositiveIntegerField(default=0)
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
 
-    phoned = models.BooleanField(default=False)
-    source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
 
-    profile_picture = models.ImageField(blank=True, null=True)
-    special_files = models.FileField(blank=True, null=True)
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
